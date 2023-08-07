7f95e777d2e6bfc2c37eab7d44e5a1bdff547c88
vishal <vishalps2606@gmail.com>
2023-08-07 18:54:55 +0530

    setup tailwind and angular/fire

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'password-manager-44f23',
+    appId: '1:733646465824:web:f617686963aa6a4e2b4545',
+    storageBucket: 'password-manager-44f23.appspot.com',
+    apiKey: 'AIzaSyDJIts9FAZlfW4-bn8epKzNjG7iOay2_Wc',
+    authDomain: 'password-manager-44f23.firebaseapp.com',
+    messagingSenderId: '733646465824',
+  },
+    production: true
+}
